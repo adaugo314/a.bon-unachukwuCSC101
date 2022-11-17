a8b3ac668c69e2fc5bb173ff34b8eaca1ca9ad7e
unknown <abon314una@gmail.com>
2022-11-17 16:49:37 +0100

    (my third commit)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+   let special_character = '@'; //default
+   let alphabet:char ='B';
+   let surname = "Bon-unachukwu";
+   let first_name = "Adaugo";
+   let middle_name = "Oluomachi";
+
+   println!(" ");
+   println!("Special character: {}",special_character);
+   println!("Alphabet: {}",alphabet);
+   println!("Name: {} {} {}",surname, middle_name, first_name);
+}
